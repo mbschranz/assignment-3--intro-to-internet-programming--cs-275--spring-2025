a499b78e66baa4b940ae4cf0f49498a1c132f8a6
mbschranz <mbschranz@gmail.com>
2025-04-10 17:34:45 -0400

    Added menu styling
    added styles for the drop down menu before the functionality in Javascript

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,15 @@
 const modal = document.querySelector(`.modal-panel`); //the modal
 const modalContent = document.querySelector(`.modal-content-pane`); //the modal panel
 const modalButton = document.getElementById(`js-triggers`).childNodes[3].childNodes[0];//the open modal button
+const menuButton = document.getElementById(`js-triggers`).childNodes[1].childNodes[0];//the open menu button
+let menuIsOpen = false;//to keep track of when the menu is open or closed
+let menuMode = true;//to keep track of whether the menu dropdown is in menu mode or sidebar mode
 
-//functions
+//MODAL FUNCTIONS
 //event listener for the open modal click
-modalButton.addEventListener(`click`, () =>{
+modalButton.addEventListener(`click`, () => {
     console.log(`modal open pressed!`);
     modal.style.top = `0`;
-
 });
 
 //event listener for modal click
@@ -32,3 +34,20 @@ modalContent.addEventListener(`click`, e => {
     console.log(`modal content panel clicked`);
     e.stopPropagation();    //prevent the modal click trigger
 });
+
+//DROPDOWN/SIDEBAR FUNCTIONS
+//event listener for opening the dropdown menu (which opens and closes it)
+menuButton.addEventListener(`click`, () => {
+    console.log(`menu button pressed`);
+});
+
+//keep track of the window size
+window.onresize = function(){
+    if(window.innerWidth < 736){
+        //left sidebar mode
+    }
+    else
+    {
+        //top menu mode
+    }
+  }
